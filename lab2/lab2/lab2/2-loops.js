683d4e4f584aca65f6d3316f3792ea81f8fb5d32
awwwixy <viktoriaslyviuk@gmail.com>
2025-10-14 15:53:54 +0300

    Update 2-loops.js

--- lab2/lab2/lab2/2-loops.js
+++ lab2/lab2/lab2/2-loops.js
@@ -1,1 +1,17 @@
+function range(start, end) {  // створює масив чисел від start до end
+ let array = [];
+    for (let i = start; i <= end; i++)
+        array.push(i);
+    return array;
+}
+console.log(range(15, 30));
 
+function rangeOdd(start, end) {  // масив лише непарних чисел
+ let array = [];
+    for (let i = start; i <= end; i++)
+        if (i % 2 === 1) {
+            array.push(i);
+           }
+    return array;
+}
+console.log(rangeOdd(15, 30));
