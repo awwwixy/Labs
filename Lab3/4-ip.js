8a0cf46b4029595615b5089ef6fae715506185bd
awwwixy <viktoriaslyviuk@gmail.com>
2025-10-18 21:44:38 +0300

    Create 4-ip.js

---
+++ Lab3/4-ip.js
@@ -0,0 +1,17 @@
+function introspectFunctions(iface) {
+  const result = [];
+  for (let key in iface) {
+    if (typeof iface[key] === 'function') {
+      result.push([key, iface[key].length]);
+    }
+  }
+  return result;
+}
+
+const iface = {
+  m1: x => [x],
+  m2: function (x, y) { return [x, y]; },
+  m3(x, y, z) { return [x, y, z]; }
+};
+
+console.log(introspectFunctions(iface));
