dd6fd5fd40c2bc4886643dac6aa8824829ef9d27
awwwixy <viktoriaslyviuk@gmail.com>
2025-12-02 00:23:26 +0200

    Update 1-seq.js
    Added a seq function to compose multiple functions.

--- Lab5/1-seq.js
+++ Lab5/1-seq.js
@@ -1,14 +1,16 @@
 "use strict";
 
-function map(array, fn) {
-  const result = [];
-  for (let item of array) {
-    result.push(fn(item));
-  }
-  return result;
+function seq(...fns) {
+  return function(value) {
+    let result = value;
+    for (let fn of fns) {
+      result = fn(result);
+    }
+    return result;
+  };
 }
 
-const nums = [1, 2, 3, 4];
-const doubled = map(nums, x => x * 2);
-
-console.log(doubled);
+// приклади
+console.log(seq(x => x + 7, x => x * 2)(5)); // 17
+console.log(seq(x => x * 2, x => x + 7)(5)); // 24
+console.log(seq(x => x + 1, x => x - 2, x => x * 3, x => x - 4)(7)); // 3
