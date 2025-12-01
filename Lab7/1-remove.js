87ad35c948217e1b16395f480444e89eb0b552bd
awwwixy <viktoriaslyviuk@gmail.com>
2025-12-02 00:14:12 +0200

    Update 1-remove.js

--- Lab7/1-remove.js
+++ Lab7/1-remove.js
@@ -1,15 +1,13 @@
 "use strict";
 
-const list = [1, 2, 3, 4, 5, 6, 7];
+const arr = [1, 2, 3, 4, 5, 6, 7];
 
-function deleteItem(arr, target) {
-  const pos = arr.indexOf(target);
-  if (pos >= 0) {
-    arr.splice(pos, 1);
+function removeElement(a, value) {
+  const i = a.indexOf(value);
+  if (i !== -1) {
+    a.splice(i, 1);
   }
 }
 
-deleteItem(list, 5);
-console.log(list);
-
-module.exports = { deleteItem };
+removeElement(arr, 5);
+console.log(arr);
