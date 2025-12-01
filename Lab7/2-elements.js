dce2ea6f9a593e1ce8145374c9aaf13a45b66069
awwwixy <viktoriaslyviuk@gmail.com>
2025-12-02 00:00:35 +0200

    Create 2-elements.js
    Implement function to remove specified elements from an array.

---
+++ Lab7/2-elements.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const array = [1, 2, 3, 4, 5, 6, 7];
+
+const removeElements = (array, ...items) => {
+  for (const item of items) {
+    const index = array.indexOf(item);
+    if (index !== -1) array.splice(index, 1);
+  }
+};
+
+removeElements(array, 5, 1, 6);
+console.log(array);
