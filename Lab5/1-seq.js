91fac24ef0f8f5cd4d44ec10418d9f037fe548ca
awwwixy <viktoriaslyviuk@gmail.com>
2025-12-02 00:19:04 +0200

    Update 1-seq.js

--- Lab5/1-seq.js
+++ Lab5/1-seq.js
@@ -1,20 +1,14 @@
 "use strict";
 
-function seq(...args) {
-  const functions = [...args];
-
-  function chain(value) {
-    if (typeof value === "number") {
-      return functions.reduceRight((result, fn) => fn(result), value);
-    } 
-    else if (typeof value === "function") {
-      functions.push(value);
-      return chain;
-    }
+function map(array, fn) {
+  const result = [];
+  for (let item of array) {
+    result.push(fn(item));
   }
-
-  return chain;
+  return result;
 }
-console.log(seq((x) => x + 7)((x) => x * 2)(5)); 
-console.log(seq((x) => x * 2)((x) => x + 7)(5)); 
-console.log(seq((x) => x + 1)((x) => x * 2)((x) => x / 3)((x) => x - 4)(7));
+
+const nums = [1, 2, 3, 4];
+const doubled = map(nums, x => x * 2);
+
+console.log(doubled);
